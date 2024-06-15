1f8fbf02fa93f65b6fd4ffc63f695e27f86f32f4
alexalcon <alexalcon1596@gmail.com>
2024-06-15 04:27:30 +0000

    Fix launch file executable parameter error

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -5,7 +5,7 @@ def  generate_launch_description():
     return LaunchDescription([
         Node(
             package='robot_patrol',
-            executable='patrol',
+            executable='patrol_node',
             output='screen',
             emulate_tty=True)
     ])
